d196021a82efe50882f1bf105af2b957f1c9df5c
Bader <elkorgliplay@gmail.com>
2025-12-29 23:29:47 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  output: 'export',      // Essential for generating the 'out' folder for IPFS
+  images: {
+    unoptimized: true,   // Required for GIF and PNG support on decentralized networks
+  },
+  trailingSlash: true,   // Optimizes URL routing within IPFS gateways
 };
 
 export default nextConfig;

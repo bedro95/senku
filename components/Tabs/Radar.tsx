9db62e9c37c6ae511d4a5c6a07bf343d5babea44
Bader <elkorgliplay@gmail.com>
2026-01-12 04:21:41 +0200

    Update Radar.tsx

--- components/Tabs/Radar.tsx
+++ components/Tabs/Radar.tsx
@@ -1,1 +1,13 @@
+import React from 'react';
+
+const RadarTab = () => {
+  return (
+    <div className="p-4">
+      {/* محتوى الـ Radar هنا */}
+      <h2 className="text-neon-green">Radar System</h2>
+    </div>
+  );
+};
+
+export default RadarTab; // هذا هو السطر الأهم الذي يحل مشكلة "is not a module"
 
